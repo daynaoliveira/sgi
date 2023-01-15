711ebcbb99cd48bafdd95fb4cc94e9672f6866dc
Dayná <nah.olliiver.p@gmail.com>
2023-01-15 15:03:17 -0400

    Alterações index login

--- web/src/views/Login/index.tsx
+++ web/src/views/Login/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/medical_research.svg';
 import './style.css';
@@ -9,49 +8,42 @@ import { faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons';
 
 function Login(){
     return(
-        <div id="page-login">
-            <div className="container">
-                <div className="form-container">
-                    <div className="signin">
-                        <h1 className="title">SGI - Login</h1>
-                        <h3 className="subtitle">Colaborador</h3>
-                        <form action="#" className="signin-form">
-                            
-                            <div className="input-field">
-                                <i>
-                                    <FontAwesomeIcon icon={faIdCard} />
-                                </i>
-                                <input type="text" name='cpf' placeholder='000.000.000-00'/>
-                            </div>
-                            <div className="input-field">
-                                <i>
-                                    <FontAwesomeIcon icon={faUnlockKeyhole} />
-                                </i>
-                                <input type="password" placeholder='********'/>
-                            </div>
-                            <Link to='/retrieve-password' className="redir" id="retrieve-password">Esqueceu sua senha?</Link>
-                            <div className="button-field">
-                                <button type="submit" className='btn-login'>
-                                    <span className='button-icon'>
-                                        <i>
-                                            <FontAwesomeIcon icon={faArrowRightToBracket} />
-                                        </i>
-                                    </span>
-                                    <span className='button-text'>Login</span>
-                                </button>
-                            </div>
-                        </form>
-                    </div>
+        <div className="container">
+            <div className="panel-container">
+                <div className="content">
+                    <h3>Bem vindo!</h3>
+                    <p>Faça login para acessar as funcionalidades do sistema</p>
+                    <img src={logoImg} alt="Logo" className='image' />
                 </div>
-                <div className="panel-container">
-                    <div className="panel left-panel">
-                        <div className="content">
-                            <h3>Bem vindo!</h3>
-                            <p>Faça login para acessar as funcionalidades do sistema</p>
-                            <img src={logoImg} alt="Logo" className='image' />
-                        </div>
+            </div>
+            <div className="form-container">
+                <h1 className="title">SGI - Login</h1>
+                <h3 className="subtitle">Colaborador</h3>
+                <form action="#" className="signin-form">
+                    <div className="input-field">
+                        <i>
+                            <FontAwesomeIcon icon={faIdCard} />
+                        </i>
+                        <input type="text" name='cpf' placeholder='000.000.000-00'/>
                     </div>
-                </div>
+                    <div className="input-field">
+                        <i>
+                            <FontAwesomeIcon icon={faUnlockKeyhole} />
+                        </i>
+                        <input type="password" placeholder='********'/>
+                    </div>
+                    <Link to='/retrieve-password' className="redir" id="retrieve-password">Esqueceu sua senha?</Link>
+                    <div className="button-field">
+                        <button type="submit" className='btn-login'>
+                            <span className='button-icon'>
+                                <i>
+                                    <FontAwesomeIcon icon={faArrowRightToBracket} />
+                                </i>
+                            </span>
+                            <span className='button-text'>Login</span>
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     );
